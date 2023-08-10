af91562e0882d53670e3a3ea1fa1c825cd151a9c
npokrandt <npokrandt919@gmail.com>
2023-08-10 00:59:27 -0500

    completes Shape class and runs successful tests on it

--- tests/shape.test.js
+++ tests/shape.test.js
@@ -1,1 +1,25 @@
 //will test that the render method is only called on the child shapes, not the parent shape
+const Shape = require('../lib/shape')
+
+describe('Shape Class', () => {
+    it('throws an error when render is called directly', () => {
+        const color = 'green'
+        const text = 'BRS'
+        const textColor = 'white'
+
+        const shape = new Shape(color, text, textColor)
+        const shapeRender = () => shape.render()
+        expect(shapeRender).toThrow(new Error('Child class must implement render() method.'))
+    })
+    it('renders the text properly when the addText function is called', () => {
+        const color = 'green'
+        const text = 'BRS'
+        const textColor = 'white'
+
+        const testXML = '<text x="150" y="120" font-size="60" text-anchor="middle" fill="white">BRS</text>'
+
+        const shape = new Shape(color, text, textColor)
+        const shapeAddText =  shape.addText()
+        expect(shapeAddText).toBe(testXML)
+    })
+})

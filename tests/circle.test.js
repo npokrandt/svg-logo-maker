3df506bb2c3dd2d79315f244b750cb095349a1bd
npokrandt <npokrandt919@gmail.com>
2023-08-11 13:31:13 -0500

    defined classes for circles and rectangles, and the tests to run on them

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -1,2 +1,33 @@
 //will test that the circle renders correctly
+const Circle = require('../lib/circle')
+
+describe('Circle Class', () => {
+    it('renders correctly when render() is called', () => {
+        const color = 'green'
+        const text = 'BRS'
+        const textColor = 'white'
+
+        const circleXML = `<circle cx="150" cy="100" r="80" fill="green" />`
+
+        const circle = new Circle(color, text, textColor)
+
+        const circleRender = circle.render()
+
+        expect(circleRender).toBe(circleXML)
+
+        // return `<circle cx="150" cy="100" r="80" fill="${this.color}" />`
+    })
+    //test that child shapes can render text with the parent's function
+    it('renders the text properly when the addText function is called', () => {
+        const color = 'green'
+        const text = 'BRS'
+        const textColor = 'white'
+
+        const testXML = '<text x="150" y="120" font-size="60" text-anchor="middle" fill="white">BRS</text>'
+        const circle = new Circle(color, text, textColor)
+        const circleAddText = circle.addText()
+        expect(circleAddText).toBe(testXML)
+    })
+}
+)
 
--- tests/rectangle.test.js
+++ tests/rectangle.test.js
@@ -1,1 +1,31 @@
 //will test that the rectangle renders correctly
+const Rectangle = require('../lib/Rectangle')
+
+describe('Rectangle Class', () => {
+    it('renders correctly when render() is called', () => {
+        const color = 'green'
+        const text = 'BRS'
+        const textColor = 'white'
+
+        const rectXML = `<rect x="50" y="35" width="200" height="130" fill="green" />`
+
+        const rectangle = new Rectangle(color, text, textColor)
+
+        const rectRender = rectangle.render()
+
+        expect(rectRender).toBe(rectXML)
+
+    })
+    //test that child shapes can render text with the parent's function
+    it('renders the text properly when the addText function is called', () => {
+        const color = 'green'
+        const text = 'BRS'
+        const textColor = 'white'
+
+        const testXML = '<text x="150" y="120" font-size="60" text-anchor="middle" fill="white">BRS</text>'
+        const rectangle = new Rectangle(color, text, textColor)
+        const rectAddText = rectangle.addText()
+        expect(rectAddText).toBe(testXML)
+    })
+}
+)
